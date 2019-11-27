32f5f75ba41703e517f2f8b79768046cef14f415
blopperman <basov.leo@googlemail.com>
2019-11-27 21:14:32 +0100

    Modified jumping

--- player/FallState.gd
+++ player/FallState.gd
@@ -3,6 +3,15 @@ extends "res://ai/FiniteState.gd"
 func update(object):
 	object.get_node("AnimatedSprite").animation = "fall"
 	
+	if Input.is_action_pressed("ui_left"):
+		object.get_node("AnimatedSprite").flip_h = true
+		object.velocity.x = -object.WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		object.get_node("AnimatedSprite").flip_h = false
+		object.velocity.x = object.WALK_SPEED
+	else:
+		object.velocity.x = 0.0
+	
 	if object.velocity.y == 0.0:
 		emit_signal("pop")
 		emit_signal("push", "IdleState")
