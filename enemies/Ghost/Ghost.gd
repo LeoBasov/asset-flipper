3d633602a6c3fe84efbce99271baac6552367eda
Leo Basov <basov_leo@web.de>
2019-11-28 09:53:43 +0100

    Added hitting.

--- enemies/Ghost/Ghost.gd
+++ enemies/Ghost/Ghost.gd
@@ -8,21 +8,21 @@ func _ready():
 func reset():
 	alive = true
 	$AnimatedSprite.animation = "idle"
-	collision_layer = 1
+	collision_layer = 2
 	collision_mask = 1
 	show()
 
 func _process(delta):
 	$AnimatedSprite.play()
 	
-func _physics_process(delta):
-	move_and_slide(Vector2(0, 0), Vector2(0, -1))
-	
-	for i in get_slide_count():
-		collision_layer = 2
-		collision_mask = 2
-		$AnimatedSprite.animation = "death"
-		alive = false
+func hit():
+	die()
+		
+func die():
+	collision_layer = 20
+	collision_mask = 20
+	$AnimatedSprite.animation = "death"
+	alive = false
 
 func _on_AnimatedSprite_animation_finished():
 	if !alive:
--- player/AttackState.gd
+++ player/AttackState.gd
@@ -13,12 +13,12 @@ func update(object):
 	animation_running = true
 	object.get_node("AnimatedSprite").animation = "attack-2"
 	
-	if attack_object_back:
+	if attack_object_front:
 		if object.get_node("AnimatedSprite").frame == 3:
-			print(object.get_node("AnimatedSprite").frame)
-	elif attack_object_front:
+			attack(attack_object_front)
+	elif attack_object_back:
 		if object.get_node("AnimatedSprite").frame == 3:
-			print(object.get_node("AnimatedSprite").frame)
+			attack(attack_object_back)
 	
 	object.get_node("AnimatedSprite").play()
 
@@ -29,10 +29,12 @@ func _on_AnimatedSprite_animation_finished():
 		emit_signal("push", "IdleState")
 		
 func attack(object):
-	pass
+	for enenmy in get_tree().get_nodes_in_group("Enemies"):
+		if enenmy.name == object.name:
+			enenmy.hit()
 
 func _on_Attack2AreaFront_body_entered(body):
-	attack_object_front = body
+ 	attack_object_front = body
 
 
 func _on_Attack2AreaFront_body_exited(body):
