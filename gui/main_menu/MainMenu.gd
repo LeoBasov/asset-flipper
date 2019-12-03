4559a9ea22cad526525c3495ced15a89ebdd1517
blopperman <basov.leo@googlemail.com>
2019-12-03 21:17:33 +0100

    Finished title screen

--- gui/main_menu/MainMenu.gd
+++ gui/main_menu/MainMenu.gd
@@ -1,10 +1,31 @@
 extends MarginContainer
 
-var index = 0
+var index : int = 0
+var multiplicator : float = 1.0
+var color : float = 0.0
 
 func _ready():
-	pass
+	color = 0.0
 
 func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
+	color += delta*multiplicator
+	
+	if color >= 1.0 or color <= 0.0:
+		multiplicator *= -1
+	
+	if Input.is_action_pressed("ui_up") and index > 0:
+		index -= 1
+	elif Input.is_action_pressed("ui_down") and index < 1:
+		index += 1
+	
+	if Input.is_action_pressed("ui_accept") and index == 0:
 		get_tree().change_scene("res://Main.tscn")
+	elif Input.is_action_pressed("ui_accept") and index == 1:
+		get_tree().quit()
+		
+	if index == 0:
+		$VBoxContainer/NewGame.modulate = Color(color, 0, 0)
+		$VBoxContainer/Quit.modulate = Color(1, 1, 1)
+	elif index == 1:
+		$VBoxContainer/NewGame.modulate = Color(1, 1, 1)
+		$VBoxContainer/Quit.modulate = Color(color, 0, 0)
