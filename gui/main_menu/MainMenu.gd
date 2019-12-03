e24cfcfa1a1bbe16f22c78dbc543c0e32f13b747
blopperman <basov.leo@googlemail.com>
2019-12-03 21:53:30 +0100

    Finished menu select sounds

--- gui/main_menu/MainMenu.gd
+++ gui/main_menu/MainMenu.gd
@@ -3,8 +3,10 @@ extends MarginContainer
 var index : int = 0
 var multiplicator : float = 1.0
 var color : float = 0.0
+var played : bool
 
 func _ready():
+	played = false
 	color = 0.0
 
 func _process(delta):
@@ -13,10 +15,22 @@ func _process(delta):
 	if color >= 1.0 or color <= 0.0:
 		multiplicator *= -1
 	
-	if Input.is_action_pressed("ui_up") and index > 0:
-		index -= 1
-	elif Input.is_action_pressed("ui_down") and index < 1:
-		index += 1
+	if Input.is_action_pressed("ui_up"):
+		if index > 0:
+			index -= 1
+			$MenuSelect.play()
+			played = false
+		elif !$MenuSelect.playing and !$MenuSelectBlock.playing and !played:
+			played = true
+			$MenuSelectBlock.play()
+	elif Input.is_action_pressed("ui_down"):
+		if index < 1:
+			index += 1
+			$MenuSelect.play()
+			played = false
+		elif !$MenuSelect.playing and !$MenuSelectBlock.playing and !played:
+			played = true
+			$MenuSelectBlock.play()
 	
 	if Input.is_action_pressed("ui_accept") and index == 0:
 		get_tree().change_scene("res://Main.tscn")
