dab0a5074651218bc0661f5a278cc099dfec6ea9
blopperman <basov.leo@googlemail.com>
2019-12-07 12:41:50 +0100

    Added stuff

--- enemies/Demon/Demon.gd
+++ enemies/Demon/Demon.gd
@@ -1,12 +1,21 @@
 extends KinematicBody2D
 
+export var fly_speed = 200
+export var max_live : float = 10
+
 var player
+var velocity = Vector2(0.0, 0.0)
+var damage : bool
+var live : float
 
 func _ready():
 	reset()
 	
 func reset():
 	player = null
+	velocity = Vector2(0.0, 0.0)
+	damage = false
+	live = max_live
 	
 	$FSM.pop_state()
 	$FSM.push_state($FSM/IdleState)
